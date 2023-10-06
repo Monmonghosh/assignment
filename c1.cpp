8824787365aa6a8e2f9f654d02aa21049d73dac9
monmon ghosh <145142071+Monmonghosh@users.noreply.github.com>
2023-10-06 20:37:14 +0530

    Add files via upload

--- c1.cpp
+++ c1.cpp
@@ -1,26 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int rows=5;
-    for(int i=0;i<2*rows-1;i++)
-    {
-        int a;
-        if(i<rows)
-        {
-            a=2*i+1;
-        }
-        else
-        {
-            a=2*(2*rows-i)-3;
-        }
-        for(int j=0;j<a;j++)
-        {
-            printf(" ");
-        }
-        for(int k=0;k<2*rows-a;k++)
-        {
-            printf(" *");
-        }
-        printf("\n");
-    }
+int row=5;
+for(int i=0;i<row;i++)
+{
+for(int j=0;j<row;j++)
+{
+if(i>0 && i<row-1 && j>0 && j<row-1)
+printf(" ");
+else
+printf("*");
+}
+printf("\n");
+}
 }
