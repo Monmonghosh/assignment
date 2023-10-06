28d54a46e0ede49e56239b60bfa31c984e300016
monmon ghosh <145142071+Monmonghosh@users.noreply.github.com>
2023-10-06 20:22:09 +0530

    Add files via upload

--- c1.cpp
+++ c1.cpp
@@ -1,13 +1,26 @@
 #include<stdio.h>
-int main() {
-    int i, j,n;
-    printf(" enter the number rows\n");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+int main()
+{
+    int rows=5;
+    for(int i=0;i<2*rows-1;i++)
     {
-    for(j=1;j<=i;j++) {
-        printf("%d", j);
-    }
-    printf("\n");
+        int a;
+        if(i<rows)
+        {
+            a=2*i+1;
+        }
+        else
+        {
+            a=2*(2*rows-i)-3;
+        }
+        for(int j=0;j<a;j++)
+        {
+            printf(" ");
+        }
+        for(int k=0;k<2*rows-a;k++)
+        {
+            printf(" *");
+        }
+        printf("\n");
     }
 }
