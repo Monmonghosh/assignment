c4733950d69fcab133b07b0710dc8576ae0743b9
monmon ghosh <145142071+Monmonghosh@users.noreply.github.com>
2023-10-11 00:08:32 +0530

    Add files via upload

---
+++ ass6.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float s,a,b,c,area,e,f,g,h;
+    printf("Enter the values of A,B and C:\n");
+    scanf("%f%f%f",&a,&b,&c);
+    s=(a+b+c)/2;
+    e=s-a;
+    f=s-b;
+    g=s-c;
+    h=s*e*f*g;
+    area=sqrt(h);
+    printf("Area of triangle is:%f\n",area);
+}
+
